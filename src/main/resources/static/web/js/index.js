7bf04f20bc3c64a772459ff6e83c98d42bfad23d
matiasutn2019 <matiasceviniydejesus@gmail.com>
2022-08-29 15:10:12 -0300

    login bad credentials catch added

--- src/main/resources/static/web/js/index.js
+++ src/main/resources/static/web/js/index.js
@@ -25,7 +25,20 @@ createApp({
                     .post('/api/login', 'email=' + this.emailLogin + '&password=' + this.passwordLogin,
                         { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
                     .then(response => {
-                        window.location.href = "/web/accounts.html";
+                        if (response.status === 200) {
+                            window.location.href = "/web/accounts.html";
+                        }
+                    })
+                    .catch(error => {
+                        if (error.response.data.exception === 'No value present') {
+                            alert('Code: ' + error.response.status + '\nUsername not found');
+                            this.emailLogin = ''
+                        } else if (error.response.data.exception === 'Bad credentials') {
+                            alert('Code: ' + error.response.status + '\nPassword incorrect');
+                            this.passwordLogin = ''
+                        } else if (error.response.data.exception === 'Maximum sessions of 1 for this principal exceeded') {
+                            alert('Code: ' + error.response.status + '\nYou are already logged in');
+                        }
                     })
             }
         },
@@ -41,6 +54,9 @@ createApp({
                         this.emailLogin = this.email;
                         this.passwordLogin = this.password;
                         this.login()
+                    })
+                    .catch(error => {
+                        alert('Code: ' + error.response.status + '\n' + error.response.data);
                     })
             }
         }
