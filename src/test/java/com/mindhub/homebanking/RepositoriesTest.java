47d85fb8a3dffd34543d16b5fab47b8e82c0a28e
matiasutn2019 <matiasceviniydejesus@gmail.com>
2022-09-06 12:47:16 -0300

    integration tests

--- src/test/java/com/mindhub/homebanking/RepositoriesTest.java
+++ src/test/java/com/mindhub/homebanking/RepositoriesTest.java
@@ -1,11 +1,11 @@
 package com.mindhub.homebanking;
 
-import com.mindhub.homebanking.models.Loan;
-import com.mindhub.homebanking.repositories.LoanRepository;
+import com.mindhub.homebanking.models.*;
+import com.mindhub.homebanking.repositories.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
@@ -13,12 +13,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = NONE)
+//@DataJpaTest // sólo si se usa postgresql
+@AutoConfigureTestDatabase(replace = NONE) // con h2 sí, postgresql no se
+// las anot. de arriba indican a Spring que debe escanear en busca de clases @Entity y configurar los repositorios JPA
+@SpringBootTest // sólo si se usa h2
 public class RepositoriesTest {
 
     @Autowired
     private LoanRepository loanRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private CardRepository cardRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     @Test
     public void existLoans() {
@@ -29,6 +43,54 @@ public class RepositoriesTest {
     @Test
     public void existPersonalLoan() {
         List<Loan> loans = loanRepository.findAll();
-        assertThat(loans, hasItem(hasProperty("name", is("Personal"))));
+        assertThat(loans, hasItem(hasProperty("name", is("PERSONAL"))));
+    }
+
+    @Test
+    public void existAccounts() {
+        List<Account> accounts = accountRepository.findAll();
+        assertThat(accounts,is(not(empty())));
+    }
+
+    @Test
+    public void existSingleAccount() {
+        List<Account> accounts = accountRepository.findAll();
+        assertThat(accounts, hasItem(hasProperty("number", is("VIN-001"))));
+    }
+
+    @Test
+    public void existCards() {
+        List<Card> cards = cardRepository.findAll();
+        assertThat(cards,is(not(empty())));
+    }
+
+    @Test
+    public void existSingleCard() {
+        List<Card> cards = cardRepository.findAll();
+        assertThat(cards, hasItem(hasProperty("number", is("0000 1111 2222 3333"))));
+    }
+
+    @Test
+    public void existClients() {
+        List<Client> clients = clientRepository.findAll();
+        assertThat(clients,is(not(empty())));
+    }
+
+    @Test
+    public void existSingleClient() {
+        List<Client> clients = clientRepository.findAll();
+        assertThat(clients, hasItem(hasProperty("email", is("melba@gmail.com"))));
+    }
+
+    @Test
+    public void existsTransactions() {
+        List<Transaction> transactions = transactionRepository.findAll();
+        assertThat(transactions,is(not(empty())));
+    }
+
+    @Test
+    public void existSingleTransaction() {
+        List<Transaction> transactions = transactionRepository.findAll();
+        assertThat(transactions, hasItem(hasProperty("description", is("crédito de $ 1000 a la cuenta 1"))));
     }
 }
